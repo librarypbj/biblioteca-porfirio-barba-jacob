600441b823fcb9aba83dd6583a3c1447d96f36ee
TuUsuarioDeGitHub <tu_correo@ejemplo.com>
2026-09-14 21:02:16 -0500

    nombre cambiado

--- app.py
+++ app.py
@@ -60,7 +60,9 @@ with st.sidebar:
         ).strip()
 
 # --- 4. CUERPO PRINCIPAL DE LA PÁGINA ---
-st.title("📚 Biblioteca")
+st.title("📚 Descubre el Conocimiento")
+st.image("biblioteca.jpg", use_container_width=True)
+
 st.write("Usa el menú de la izquierda para seleccionar un género y empezar tu búsqueda.")
 st.markdown("---")
 
