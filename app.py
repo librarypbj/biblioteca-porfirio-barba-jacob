85421f18c45fd6bfc777935942ecc9316490bdaf
TuUsuarioDeGitHub <tu_correo@ejemplo.com>
2026-09-24 17:26:30 -0500

    corchete establlecido

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if "libros_db" not in st.session_state:
             "imagen_portada": "d109.jpg"
         },
     
-    {
+            {
             "codigo": "cp110",
             "titulo": "Sabotaje en el Atlantico",
             "autor": "Gordon Thomas & Max Morgan-Witts",
@@ -89,8 +89,10 @@ if "libros_db" not in st.session_state:
             "archivo_interno": "cp110.pdf",
             "imagen_portada": "cp110.jpg"
         }
+ ]
+    
 
-        ]
+      
    
 # --- FUNCIONES AUXILIARES ---
 def buscar_archivo(nombre_archivo):
