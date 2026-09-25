35748c00b090bdaf71ada5ac103e50fc7289cad3
TuUsuarioDeGitHub <tu_correo@ejemplo.com>
2026-09-24 20:19:24 -0500

    letra corregida

--- app.py
+++ app.py
@@ -252,13 +252,13 @@ if "libros_db" not in st.session_state:
             "imagen_portada": "li129.jpg"
         },
          {
-            "codigo": "li130",
+            "codigo": "i130",
             "titulo": "Tutubi Patrol 8:Si Bumbi Bubuyog at ang mga Alitaptap",
             "autor": "Timmy l. Cruz & Luis P. Gatmaitan",
             "genero": "Cuento Infantil Educativo, bilingüe",
             "disponible_fisico": True,
-            "archivo_interno": "li130.pdf",
-            "imagen_portada": "li130.jpg"
+            "archivo_interno": "i130.pdf",
+            "imagen_portada": "i130.jpg"
         }
  ]
     
